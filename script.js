48c844aece20f5189a21cfd8c84d444daf8e56ef
EXPEDITO TERTO DA SILVA NETO <etsn1@aluno.ifal.edu.br>
2026-06-20 11:35:02 -0300

    versão funcional, mas com bugs

--- script.js
+++ script.js
@@ -7,22 +7,46 @@ const visor = document.getElementById("visorText");
 
 //define a classe botão da calculadora
 class Botao {
+  //construtor da classe
   constructor(valor, id) {
     this.valor = valor;
-    this.id = id;
+    this.id = document.getElementById(id);
+
+    //aplica o evento tipo click a todos
+    this.id.addEventListener("click", () => {
+      this.acao();
+    });
+  }
+  //declara o método que posteriormente usará polimorfismo
+  acao() {
+    visor.innerHTML = visor.innerHTML + this.valor;
+  }
+}
+
+class Deletar extends Botao {
+  acao() {
+    visor.innerHTML = "";
+  }
+}
+class Igualdade extends Botao {
+  acao() {
+    let resultado = eval(visor.innerHTML);
+    visor.innerHTML = resultado;
   }
 }
 
 //array de números
 const numeros = [];
-//preenche o array
+const operadores = [];
+const operEespeciais = [];
+//preenche o array dos números
 for (let i = 0; i < 10; i++) {
-  numeros.push(new Botao(i, document.getElementById(i)));
+  numeros.push(new Botao(i, i));
 }
-
-//aplica o evento tipo click a todos
-numeros.forEach((element) => {
-  element.id.addEventListener("click", () => {
-    visor.innerHTML = visor.innerHTML + " " + element.valor;
-  });
-});
+operadores.push(
+  new Botao("+", 10),
+  new Botao("-", 11),
+  new Botao("*", 12),
+  new Botao("/", 13),
+);
+operEespeciais.push(new Deletar("DEL", 15), new Igualdade("=", 14));
